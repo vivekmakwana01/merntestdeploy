99112bd170981abbc8389a5c3cf19305b34c7b60
unknown <mvpcc999@gmail.com>
2021-04-27 19:47:38 +0530

    'init'

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8000;
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/testdeploy", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection successful.");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.get("/get", (req, res) => {
+  res.send("hello");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
